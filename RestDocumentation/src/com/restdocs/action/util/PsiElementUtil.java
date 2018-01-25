b607fd5da9d169d374765a34d0a453f313120e40
Miguel <miguel.cordova@verizon.com>
2018-01-25 11:24:59 -0500

    version 1.4

--- RestDocumentation/src/com/restdocs/action/util/PsiElementUtil.java
+++ RestDocumentation/src/com/restdocs/action/util/PsiElementUtil.java
@@ -16,17 +16,62 @@ public class PsiElementUtil {
 
         if (value instanceof PsiReferenceExpression) {
             PsiReferenceExpression expression = (PsiReferenceExpression) value;
-            values.add(expression.getText());
+            PsiElement psiElement = expression.resolve();
+
+            if(psiElement instanceof PsiField) {
+                String fieldValue = ((PsiField) psiElement).computeConstantValue().toString();
+                if (fieldValue.startsWith("PsiField:")) {
+                    values.add(fieldValue.substring(9));
+                } else {
+                    values.add(fieldValue);
+                }
+            }
         } else if (value instanceof PsiLiteralExpression) {
             values.add(psiNameValuePair.getLiteralValue());
         } else if (value instanceof PsiArrayInitializerMemberValue) {
             PsiArrayInitializerMemberValue arrayValue = (PsiArrayInitializerMemberValue) value;
 
             for (PsiAnnotationMemberValue initializer : arrayValue.getInitializers()) {
-                values.add(initializer.getText().replaceAll("\\\"", ""));
+                if (initializer instanceof PsiPolyadicExpression) {
+                    values.add(getFullUrlFromExpression((PsiPolyadicExpression) initializer));
+                } else if (initializer instanceof PsiReferenceExpression) {
+                    PsiReferenceExpression expression = (PsiReferenceExpression) initializer;
+                    PsiElement psiElement = expression.resolve();
+
+                    if(psiElement instanceof PsiField) {
+                        values.add(((PsiField) psiElement).computeConstantValue().toString());
+                    } else {
+                        values.add(expression.getText());
+                    }
+                } else {
+                    values.add(initializer.getText().replaceAll("\\\"", ""));
+                }
             }
+        } else if (value instanceof PsiPolyadicExpression) {
+            values.add(getFullUrlFromExpression((PsiPolyadicExpression) value));
+        } else {
+            values.add(value.getText());
         }
         return values;
+    }
+
+    private static String getFullUrlFromExpression(PsiPolyadicExpression polyadicExpression) {
+        PsiExpression[] operands = polyadicExpression.getOperands();
+
+        StringBuilder fullUrl = new StringBuilder("");
+
+        for (PsiExpression operand : operands) {
+            if (operand instanceof PsiReferenceExpression) {
+                PsiElement element = ((PsiReferenceExpression) operand).resolve();
+                if (element instanceof PsiField) {
+                    fullUrl.append(((PsiField) element).computeConstantValue().toString());
+                }
+            } else if (operand instanceof PsiLiteralExpression) {
+                fullUrl.append(((PsiLiteralExpression) operand).getValue());
+            }
+        }
+
+        return fullUrl.toString();
     }
 
     private static List<String> getAttributeValue(PsiNameValuePair[] attributes, String attributeName) {
